4e11d7dcbdce29da0419a199ea6afab7105f8ae7
Axel <axosar@gmail.com>
2020-01-16 11:37:59 -0600

    Updates

---
+++ precache-manifest.8dd3263cd3d47f070cf3d0fe7ab1d3a4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1727486ec7f1a0065c32e0a4d4616779",
+    "url": "/macrofibrasgcp/index.html"
+  },
+  {
+    "revision": "21875c4fef9a812fccce",
+    "url": "/macrofibrasgcp/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "904c758a1728cede220c",
+    "url": "/macrofibrasgcp/static/css/main.08e14eec.chunk.css"
+  },
+  {
+    "revision": "21875c4fef9a812fccce",
+    "url": "/macrofibrasgcp/static/js/2.0975eaa1.chunk.js"
+  },
+  {
+    "revision": "904c758a1728cede220c",
+    "url": "/macrofibrasgcp/static/js/main.322c3356.chunk.js"
+  },
+  {
+    "revision": "7268e7101e76aefa3d00",
+    "url": "/macrofibrasgcp/static/js/runtime-main.48f837aa.js"
+  },
+  {
+    "revision": "0933d9cb409bbb2b66512bc1d107f820",
+    "url": "/macrofibrasgcp/static/media/LOGOTexto.0933d9cb.svg"
+  },
+  {
+    "revision": "9b2b7ed0fe734643baf9dd2bc848aecc",
+    "url": "/macrofibrasgcp/static/media/capas_compresion.9b2b7ed0.jpg"
+  },
+  {
+    "revision": "103132bc0cf3e8092ad5fa90a94af05c",
+    "url": "/macrofibrasgcp/static/media/grace.103132bc.png"
+  },
+  {
+    "revision": "e31835d30461646e2aa086790a8fed5b",
+    "url": "/macrofibrasgcp/static/media/losas_cimentacion.e31835d3.jpg"
+  },
+  {
+    "revision": "f08dcba9aba3ded470a4d0150754d999",
+    "url": "/macrofibrasgcp/static/media/malla.f08dcba9.jpg"
+  },
+  {
+    "revision": "9be71f0d6249541853f4099a11ad0f04",
+    "url": "/macrofibrasgcp/static/media/muro_concreto.9be71f0d.jpg"
+  },
+  {
+    "revision": "c57d982a76fc7ced556f3d876a7064f2",
+    "url": "/macrofibrasgcp/static/media/piso_industrial.c57d982a.jpg"
+  },
+  {
+    "revision": "a8bc6a0847985af063cb6c387e10503b",
+    "url": "/macrofibrasgcp/static/media/prefabricados.a8bc6a08.jpg"
+  },
+  {
+    "revision": "da04727b033f6824d6e000fbdbed12d9",
+    "url": "/macrofibrasgcp/static/media/varilla.da04727b.jpg"
+  }
+]);
